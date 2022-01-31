1af2d06bf0c4d9b6e73981404569d3e07089fac1
Max Oesterle <max_oesterle@web.de>
2022-01-31 21:28:22 +0100

    Fix area parking not connected to routing graph

--- src/osm_truck_parking.cpp
+++ src/osm_truck_parking.cpp
@@ -92,8 +92,45 @@ int main(int argc, char *argv[])
 		};
 	}
 
+	BitVector is_parking_node;
+	BitVector is_parking_modelling_node;
+	{
+		log_message("Extracting parking.");
+
+		const auto parking_mapping = load_osm_parking_id_mapping_from_pbf(
+			pbf_file, has_parking_node_criteria, log_message);
+
+		auto parking = load_osm_parking_from_pbf(
+			pbf_file,
+			parking_mapping,
+			log_message);
+
+		try
+		{
+			log_message("Start saving parking information");
+			long long timer = -get_micro_time();
+
+			if (!parking_info_file.empty())
+				save_parking_tags_csv(parking_info_file, parking);
+			// if (!osm_parking_way_file.empty())
+			// 	save_bit_vector(osm_parking_way_file, parking_mapping.is_parking_way);
+
+			timer += get_micro_time();
+			log_message("Finished saving, needed " + std::to_string(timer) + "musec.");
+		}
+		catch (std::exception &err)
+		{
+			std::cout << "Exception : " << err.what() << std::endl;
+		}
+
+		is_parking_node = parking_mapping.is_parking_node;
+		is_parking_modelling_node = parking_mapping.is_parking_modelling_node;
+	}
+
 	BitVector is_routing_node;
-	BitVector routing_parking_flags;
+	BitVector is_area_parking_node;
+	OSMRoutingGraph routing_graph;
+	std::vector<uint32_t> travel_time;
 	{
 		auto mapping = load_osm_id_mapping_from_pbf(
 			pbf_file,
@@ -105,7 +142,9 @@ int main(int argc, char *argv[])
 		std::vector<uint32_t> way_speed(routing_way_count);
 		std::vector<std::string> way_name(routing_way_count);
 
-		auto routing_graph = load_osm_routing_graph_from_pbf(
+		mapping.is_routing_node = mapping.is_routing_node | (mapping.is_modelling_node & is_parking_modelling_node);
+
+		routing_graph = load_osm_routing_graph_from_pbf(
 			pbf_file,
 			mapping,
 			[&](uint64_t osm_way_id, unsigned routing_way_id, const TagMap &way_tags)
@@ -119,7 +158,7 @@ int main(int argc, char *argv[])
 
 		unsigned arc_count = routing_graph.arc_count();
 
-		std::vector<uint32_t> travel_time = routing_graph.geo_distance;
+		travel_time = routing_graph.geo_distance;
 		for (unsigned a = 0; a < arc_count; ++a)
 		{
 			travel_time[a] *= 18000;
@@ -136,40 +175,39 @@ int main(int argc, char *argv[])
 				osm_node_ids.push_back(i);
 		}
 
-		{
-			log_message("Start saving routing graph");
-			long long timer = -get_micro_time();
+		log_message("Start saving routing graph");
+		long long timer = -get_micro_time();
 
-			if (!first_out_file.empty())
-				save_vector(first_out_file, routing_graph.first_out);
-			if (!head_file.empty())
-				save_vector(head_file, routing_graph.head);
-			// if (!geo_distance_file.empty())
-			// 	save_vector(geo_distance_file, routing_graph.geo_distance);
-			if (!travel_time_file.empty())
-				save_vector(travel_time_file, travel_time);
-			// if (!way_file.empty())
-			// 	save_vector(way_file, routing_graph.way);
-			// if (!way_name_file.empty())
-			// 	save_vector(way_name_file, way_name);
-			// if (!way_speed_file.empty())
-			// 	save_vector(way_speed_file, way_speed);
-			if (!latitude_file.empty())
-				save_vector(latitude_file, routing_graph.latitude);
-			if (!longitude_file.empty())
-				save_vector(longitude_file, routing_graph.longitude);
-			if (!osm_node_id_file.empty())
-				save_vector(osm_node_id_file, osm_node_ids);
-			// if (!osm_way_file.empty())
-			// 	save_bit_vector(osm_way_file, mapping.is_routing_way);
+		if (!first_out_file.empty())
+			save_vector(first_out_file, routing_graph.first_out);
+		if (!head_file.empty())
+			save_vector(head_file, routing_graph.head);
+		// if (!geo_distance_file.empty())
+		// 	save_vector(geo_distance_file, routing_graph.geo_distance);
+		if (!travel_time_file.empty())
+			save_vector(travel_time_file, travel_time);
+		// if (!way_file.empty())
+		// 	save_vector(way_file, routing_graph.way);
+		// if (!way_name_file.empty())
+		// 	save_vector(way_name_file, way_name);
+		// if (!way_speed_file.empty())
+		// 	save_vector(way_speed_file, way_speed);
+		if (!latitude_file.empty())
+			save_vector(latitude_file, routing_graph.latitude);
+		if (!longitude_file.empty())
+			save_vector(longitude_file, routing_graph.longitude);
+		if (!osm_node_id_file.empty())
+			save_vector(osm_node_id_file, osm_node_ids);
+		// if (!osm_way_file.empty())
+		// 	save_bit_vector(osm_way_file, mapping.is_routing_way);
 
-			timer += get_micro_time();
-			log_message("Finished saving, needed " + std::to_string(timer) + "musec.");
-		}
+		timer += get_micro_time();
+		log_message("Finished saving, needed " + std::to_string(timer) + "musec.");
 
-		routing_parking_flags.make_large_enough_for(routing_graph.node_count());
 		is_routing_node = mapping.is_routing_node;
-
+		is_area_parking_node = mapping.is_modelling_node & is_parking_modelling_node;
+	}
+	{
 		log_message("Start building CH.");
 
 		long long timer = -get_micro_time();
@@ -221,56 +259,25 @@ int main(int argc, char *argv[])
 		// std::string ch_file = "ch";
 		// if (!ch_file.empty())
 		// 	ch.save_file(ch_file);
-
-		std::cout << "Test query" << std::endl;
-		ContractionHierarchyQuery ch_query(ch);
-		ch_query.add_source(6538).add_target(142540).run();
-		auto distance = ch_query.get_distance();
-		std::cout << "dist is " << distance << std::endl;
-		std::cout << "middle node  is " << ch_query.shortest_path_meeting_node << std::endl;
 	}
-
-	log_message("Extracting parking.");
-
-	const auto parking_mapping = load_osm_parking_id_mapping_from_pbf(
-		pbf_file, has_parking_node_criteria, log_message);
-
-	auto parking = load_osm_parking_from_pbf(
-		pbf_file,
-		parking_mapping,
-		log_message);
 
 	log_message("Constructing parking flags");
 	LocalIDMapper routing_node_mapper(is_routing_node);
+	BitVector routing_parking_flags;
+	routing_parking_flags.make_large_enough_for(routing_graph.node_count());
 
-	for (uint64_t i = 0; i < parking_mapping.is_parking_node.size(); ++i)
+	for (uint64_t i = 0; i < is_parking_node.size(); ++i)
 	{
-		if (parking_mapping.is_parking_node.is_set(i))
+		if (is_parking_node.is_set(i) || is_area_parking_node.is_set(i))
 		{
 			routing_parking_flags.set(routing_node_mapper.to_local(i));
 		}
 	}
+
 	log_message("Finished constructing parking flags");
 
-	try
-	{
-		log_message("Start saving parking information");
-		long long timer = -get_micro_time();
-
-		if (!parking_info_file.empty())
-			save_parking_tags_csv(parking_info_file, parking);
-		if (!routing_parking_flags_file.empty())
-			save_bit_vector(routing_parking_flags_file, routing_parking_flags);
-		// if (!osm_parking_way_file.empty())
-		// 	save_bit_vector(osm_parking_way_file, parking_mapping.is_parking_way);
-
-		timer += get_micro_time();
-		log_message("Finished saving, needed " + std::to_string(timer) + "musec.");
-	}
-	catch (std::exception &err)
-	{
-		std::cout << "Exception : " << err.what() << std::endl;
-	}
+	if (!routing_parking_flags_file.empty())
+		save_bit_vector(routing_parking_flags_file, routing_parking_flags);
 
 	complete_timer += get_micro_time();
 	log_message("Finished extraction, needed " + std::to_string(complete_timer) + "musec.");
